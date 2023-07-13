43610e978419ccb12f5f7a9f33141ee4725e42ed
Pedro Henrique Zacarias dos Santos <128100166+Pedro065@users.noreply.github.com>
2023-07-13 03:38:16 -0300

    login.js

--- login.js
+++ login.js
@@ -43,7 +43,7 @@ function entrar(){
   })
    
   if(usuario.value == userValid.user && senha.value == userValid.senha){
-    window.location.href = '../menu/menu.html'
+    window.location.href = 'https://pedro065.github.io/lifefit.github.io/menu.html'
     
     let mathRandom = Math.random().toString(16).substr(2)
     let token = mathRandom + mathRandom
